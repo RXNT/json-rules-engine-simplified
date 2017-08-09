e56ea950ca2f19510474ee0c698dab3a1310cca7
Anton Oparin <mavarazy@gmail.com>
2017-08-09 10:58:47 +0200

    #5

--- src/checkField.js
+++ src/checkField.js
@@ -1,40 +1,41 @@
 import predicate from "predicate";
 import { isObject } from "./utils";
 
-import { OR, AND, NOT } from "./constants";
+import { AND, NOT, OR } from "./constants";
 
-const parseObjectRule = (rule, fieldVal) => {
-  return Object.keys(rule).every(p => {
-    let subRule = rule[p];
-    if (p === OR || p === AND) {
-      if (Array.isArray(subRule)) {
-        if (p === OR) {
-          return subRule.some(rule => checkField(fieldVal, rule));
+const checkRule = (fieldVal, rule) => {
+  if (isObject(rule)) {
+    return Object.keys(rule).every(p => {
+      let subRule = rule[p];
+      if (p === OR || p === AND) {
+        if (Array.isArray(subRule)) {
+          if (p === OR) {
+            return subRule.some(rule => checkField(fieldVal, rule));
+          } else {
+            return subRule.every(rule => checkField(fieldVal, rule));
+          }
         } else {
-          return subRule.every(rule => checkField(fieldVal, rule));
+          return false;
         }
+      } else if (p === NOT) {
+        return !checkField(fieldVal, subRule);
+      } else if (predicate[p]) {
+        return predicate[p](fieldVal, subRule);
       } else {
         return false;
       }
-    } else if (p === NOT) {
-      return !checkField(fieldVal, subRule);
-    } else if (predicate[p]) {
-      return predicate[p](fieldVal, subRule);
-    } else {
-      return false;
-    }
-  });
+    });
+  } else {
+    return predicate[rule](fieldVal);
+  }
 };
 
 export default function checkField(fieldVal, rule) {
   if (Array.isArray(fieldVal)) {
-    return isObject(rule)
-      ? fieldVal.some(val => checkField(val, rule)) || parseObjectRule(rule)
-      : fieldVal.some(val => checkField(val, rule)) || predicate[rule](fieldVal);
-  } else if (isObject(rule)) {
-    // Complicated rule - like { greater then 10 }
-    return parseObjectRule(rule, fieldVal);
-  } else {
-    return predicate[rule](fieldVal);
+    let hasValidEntry = fieldVal.some(val => checkField(val, rule));
+    if (hasValidEntry) {
+      return true;
+    }
   }
+  return checkRule(fieldVal, rule);
 }
